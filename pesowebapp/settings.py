1d6fe74b6df7bc82e2774e37e74fcfbcef488c09
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2022-07-25 23:20:03 +0800

    Update settings.py

--- pesowebapp/settings.py
+++ pesowebapp/settings.py
@@ -135,9 +135,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = (
-    BASE_DIR / "staticfiles",
-)
+STATIC_ROOT = BASE_DIR / "staticfiles",
 # Base url to serve media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (BASE_DIR / 'media')
